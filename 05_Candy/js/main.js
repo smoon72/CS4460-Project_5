5f6b909a82ad711cf549442f8fb39c3dd627e112
onisight <allwither4@gmail.com>
2017-11-28 11:13:34 -0500

    cleaned up data

--- 05_Candy/js/main.js
+++ 05_Candy/js/main.js
@@ -1,19 +1,26 @@
-d3.csv('./data/candy.csv', function(error, dataset){
+dataset = null
+d3.csv('./data/us_candy.csv', function(error, __dataset){
     if(error) {
-        console.error('Error while loading candy csvdataset.');
+        console.error('Error while loading candy csv dataset.');
         console.error(error);
         return;
     }
     // **** Your JavaScript code goes here ***
 
-    nested = d3.nest()
+    console.log(__dataset);
+    dataset = __dataset
+    data_by_state = d3.nest()
         .key(function (d) {
-            return d.Q4_COUNTRY;
+            return d.Q5_STATE_PROVINCE_COUNTY_ETC;
         })
         .entries(dataset);
 
-    console.log(nested);
-
-
-
+    console.log(data_by_state);
+    console.log("done with nesting")
+    setup();
 });
+
+function setup() {
+
+
+}
